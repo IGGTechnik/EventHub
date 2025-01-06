b008f755b620b6826a2a274b1d79aafca824d2fd
phista7 <philip@p-staudacher.de>
2025-01-06 18:36:44 +0100

    Disposing DbContext after Database migrating & formatting

--- GraphQLBackend/Program.cs
+++ GraphQLBackend/Program.cs
@@ -5,29 +5,27 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddGraphQL()
-       .AddQueryType<Query>()
-       .AddMutationType<Mutation>();
-
+builder.Services.AddGraphQLServer()
+                .AddQueryType<Query>()
+                .AddMutationType<Mutation>();
 
 var connectionString = builder.Configuration.GetConnectionString("EventHubDb");
-
 if (string.IsNullOrEmpty(connectionString))
 {
     throw new InvalidOperationException("Connection string is missing");
 }
-
 builder.Services.AddPooledDbContextFactory<EventHubDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
-
 
 var app = builder.Build();
 
-EventHubDbContext dbContext = await app.Services.CreateScope().ServiceProvider.GetRequiredService<IDbContextFactory<EventHubDbContext>>().CreateDbContextAsync();
-dbContext.Database.Migrate();
+using (IServiceScope scope = app.Services.CreateScope())
+{
+    using (EventHubDbContext dbContext = scope.ServiceProvider.GetRequiredService<IDbContextFactory<EventHubDbContext>>().CreateDbContext())
+    {
+        dbContext.Database.Migrate();
+    }
+}
 
 app.UseWebSockets();
-
 app.MapGraphQL();
-
 app.Run();
